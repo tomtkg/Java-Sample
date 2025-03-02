3d9de1cda24c17c494f48bc7211ea0cdb698431a
tomtkg, Ph.D. <tom.aquamarine@gmail.com>
2025-03-03 07:25:29 +0900

    Create BasicHelloController.java

---
+++ Practice/src/main/java/com/example/demo/BasicHelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  // REST API を作成するアノテーション
+@RequestMapping("/hello")  // URLのパスを指定
+public class BasicHelloController {
+
+	@GetMapping  // HTTP GETリクエストを処理
+	public String sayHello() {
+		return "Hello, World!";
+	}
+}
