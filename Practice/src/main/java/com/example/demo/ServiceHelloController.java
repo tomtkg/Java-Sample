c9e589d9ee1613321ee829139aa10f5a5f10ea4d
tomtkg, Ph.D. <tom.aquamarine@gmail.com>
2025-03-04 08:28:12 +0900

    Create ServiceHelloController.java

---
+++ Practice/src/main/java/com/example/demo/ServiceHelloController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello3")  // `/hello3` にアクセスしたらこのクラスが処理
+public class ServiceHelloController {
+
+    private final HelloService helloService;
+
+    // コンストラクタで HelloService を注入
+    public ServiceHelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping
+    public String sayHello(@RequestParam(defaultValue = "World") String name) {
+        return helloService.getGreeting(name);
+    }
+}
