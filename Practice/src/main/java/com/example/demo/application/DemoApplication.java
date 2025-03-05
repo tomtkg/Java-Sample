85df8bf3bc47d0aead3f39563d0450b14ab4f537
tomtkg, Ph.D. <tom.aquamarine@gmail.com>
2025-03-06 08:14:07 +0900

    Move DemoApplication.java

--- Practice/src/main/java/com/example/demo/application/DemoApplication.java
+++ Practice/src/main/java/com/example/demo/application/DemoApplication.java
@@ -1,9 +1,9 @@
-package com.example.demo;
+package com.example.demo.application;  // ファイルを移動したら package を修正
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.demo")  // すべてのサブパッケージをスキャン
 public class DemoApplication {
 
 	public static void main(String[] args) {
